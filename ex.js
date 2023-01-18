8c7ef19b0342f1dae2259f40531cfeae3cb01fcc
EleanorColvin <99199758+EleanorColvin@users.noreply.github.com>
2023-01-18 14:01:46 -0500

    Add files via upload

--- ex.js
+++ ex.js
@@ -4,11 +4,13 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello there');
 });
+
 const courses = [
-    { id: 1, name: 'Web Development' },
-    { id: 2, name: 'IT' },
-    { id: 3, name: 'Cybersecurity' },
+    { id: 1, name: "Web Development" },
+    { id: 2, name: "IT" },
+    { id: 3, name: "Cybersecurity" },
 ];
+
 // http GET requests route
 app.get('/api/courses', (req, res) => {
     res.send(courses);
@@ -22,23 +24,46 @@ app.get('/api/courses/:id', (req, res) => {
     }
     res.send(course);
 })
+
 // http POST requests
 app.post('/api/courses', (req, res) => {
-    // you write the if code here
-    //add an if statement so that the name of the course you post is .min(3) characters 
-    if (req.body.name >= 3) {
-        const course = {
-            //we assign an ID and a name property
-            id: courses.length + 1,
-            name: req.body.name
-        }
-    }
-    //YOU WRITE THE NEXT LINES OF code
-    //next step: push it to the array
-    //next step: the server should return the new resource to the client in the body of the response
-
-
+    /* if(req.body.name.length < 3)
+     {
+         res.status(404).send("The name of the course must be greater than 3 characters");
+         return
+     }*/
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 });
+
+// PUT Requests
+app.put('/api/courses/:id', (req,res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+        //Write the code in order to look up the course, if not existing return a 404
+        //otherwise 
+    if (!course) {
+        res.status(404).send("The course with the given ID was not found");
+        return
+    }
+        //update the course
+        //return the updated course
+    });    
+
+// DELETE requests
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send("The course with the given ID was not found");
+        return
+    }
+    // Use indexOf() of and splice() methods to delete the coourse by index
+    res.send(course);
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
